0e7a4fbc06ceb3c8720b0f12b8073c6a8418f59e
Eryk Lewandowski <49733053+lewy12121212@users.noreply.github.com>
2023-10-10 16:15:34 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,147 +1,151 @@
+import { Component } from 'react';
 import {
   ViewStyle,
   StyleProp,
   NativeSyntheticEvent,
   NativeScrollEvent,
-  ScrollViewProps
-} from 'react-native'
-import { Component } from 'react'
+  ScrollViewProps,
+} from 'react-native';
 
 declare module 'react-native-swiper' {
   interface SwiperState {
-    autoplayEnd: false
-    loopJump: false
-    width: number
-    height: number
+    autoplayEnd: false;
+    loopJump: false;
+    width: number;
+    height: number;
     offset: {
-      x: number
-      y: number
-    }
-    total: number
-    index: number
-    dir: 'x' | 'y'
+      x: number;
+      y: number;
+    };
+    total: number;
+    index: number;
+    dir: 'x' | 'y';
   }
 
   interface SwiperInternals extends SwiperState {
-    isScrolling: boolean
+    isScrolling: boolean;
   }
 
-  interface SwiperProps
-    extends Omit<ScrollViewProps, 'onScrollBeginDrag' | 'onMomentumScrollEnd'> {
+  interface SwiperProps extends Omit<ScrollViewProps, 'onScrollBeginDrag' | 'onMomentumScrollEnd'> {
     // Basic
     // If true, the scroll view's children are arranged horizontally in a row instead of vertically in a column.
-    horizontal?: boolean
+    horizontal?: boolean;
     // If no specify default enable fullscreen mode by flex: 1.
-    loop?: boolean
+    loop?: boolean;
     // Index number of initial slide.
-    index?: number
+    index?: number;
     // Set to true make control buttons visible.
-    showsButtons?: boolean
+    showsButtons?: boolean;
     // Set to false to disable continuous loop mode.
-    autoplay?: boolean
+    autoplay?: boolean;
     // Called with the new index when the user swiped
-    onIndexChanged?: (index: number) => void
+    onIndexChanged?: (index: number) => void;
 
     // Custom basic style & content
     // Set to true enable auto play mode.
-    width?: number
+    width?: number;
     // If no specify default fullscreen mode by flex: 1.
-    height?: number
+    height?: number;
     // See default style in source.
-    style?: StyleProp<ViewStyle>
+    style?: StyleProp<ViewStyle>;
     // Customize the View container.
-    containerStyle?: StyleProp<ViewStyle>
+    containerStyle?: StyleProp<ViewStyle>;
     // Only load current index slide , loadMinimalSize slides before and after.
-    loadMinimal?: boolean
+    loadMinimal?: boolean;
     // see loadMinimal
-    loadMinimalSize?: number
+    loadMinimalSize?: number;
     // Custom loader to display when slides aren't loaded
-    loadMinimalLoader?: React.ReactNode
+    loadMinimalLoader?: React.ReactNode;
 
     // Pagination
     // Set to true make pagination visible.
-    showsPagination?: boolean
+    showsPagination?: boolean;
     // Custom styles will merge with the default styles.
-    paginationStyle?: StyleProp<ViewStyle>
+    paginationStyle?: StyleProp<ViewStyle>;
     // Complete control how to render pagination with three params (index, total, context) ref to this.state.index / this.state.total / this, For example: show numbers instead of dots.
-    renderPagination?: (
-      index: number,
-      total: number,
-      swiper: Swiper
-    ) => React.ReactNode
+    renderPagination?: (index: number, total: number, swiper: Swiper) => React.ReactNode;
     // Allow custom the dot element.
-    dot?: React.ReactNode
+    dot?: React.ReactNode;
     // Allow custom the active-dot element.
-    activeDot?: React.ReactNode
+    activeDot?: React.ReactNode;
     // Allow custom the active-dot element.
-    dotStyle?: StyleProp<ViewStyle>
+    dotStyle?: StyleProp<ViewStyle>;
     // Allow custom the active-dot element.
-    dotColor?: string
+    dotColor?: string;
     // Allow custom the active-dot element.
-    activeDotColor?: string
+    activeDotColor?: string;
     // Allow custom the active-dot element.
-    activeDotStyle?: StyleProp<ViewStyle>
+    activeDotStyle?: StyleProp<ViewStyle>;
 
     // Autoplay
     // Delay between auto play transitions (in second).
-    autoplayTimeout?: number
+    autoplayTimeout?: number;
     // Cycle direction control.
-    autoplayDirection?: boolean
+    autoplayDirection?: boolean;
 
     // Control buttons
     // Set to true make control buttons visible.
-    buttonWrapperStyle?: StyleProp<ViewStyle>
+    buttonWrapperStyle?: StyleProp<ViewStyle>;
     // Allow custom the next button.
-    nextButton?: React.ReactNode
+    nextButton?: React.ReactNode;
     // Allow custom the prev button.
-    prevButton?: React.ReactNode
+    prevButton?: React.ReactNode;
 
     // Supported ScrollResponder
     // When animation begins after letting up
     onScrollBeginDrag?: (
       e: NativeSyntheticEvent<NativeScrollEvent>,
       state: SwiperInternals,
-      swiper: Swiper
-    ) => void
+      swiper: Swiper,
+    ) => void;
     // Makes no sense why this occurs first during bounce
     onMomentumScrollEnd?: (
       e: NativeSyntheticEvent<NativeScrollEvent>,
       state: SwiperInternals,
-      swiper: Swiper
-    ) => void
+      swiper: Swiper,
+    ) => void;
     // Immediately after onMomentumScrollEnd
-    onTouchStartCapture?: any
+    onTouchStartCapture?: any;
     // Same, but bubble phase
-    onTouchStart?: any
+    onTouchStart?: any;
     // You could hold the touch start for a long time
-    onTouchEnd?: any
+    onTouchEnd?: any;
     // When lifting up - you could pause forever before * lifting
-    onResponderRelease?: any
+    onResponderRelease?: any;
 
     // If true, the scroll view stops on multiples of the scroll view's size when scrolling. This can be used for
     // horizontal pagination.
-    pagingEnabled?: boolean
+    pagingEnabled?: boolean;
     // Set to true if you want to show horizontal scroll bar.
-    showsHorizontalScrollIndicator?: boolean
+    showsHorizontalScrollIndicator?: boolean;
     // Set to true if you want to show vertical scroll bar.
-    showsVerticalScrollIndicator?: boolean
+    showsVerticalScrollIndicator?: boolean;
     // If true, the scroll view bounces when it reaches the end of the content if the content is larger then the
     // scroll view along the axis of the scroll direction. If false, it disables all bouncing even if the
     // alwaysBounce* props are true.
-    bounces?: boolean
+    bounces?: boolean;
     // If true, the scroll view scrolls to top when the status bar is tapped.
-    scrollsToTop?: boolean
+    scrollsToTop?: boolean;
     // If true, offscreen child views (whose overflow value is hidden) are removed from their native backing
     // superview when offscreen. This canimprove scrolling performance on long lists.
-    removeClippedSubviews?: boolean
+    removeClippedSubviews?: boolean;
     // Set to true if you need adjust content insets automation.
-    automaticallyAdjustContentInsets?: boolean
+    automaticallyAdjustContentInsets?: boolean;
     // Enables/Disables swiping
-    scrollEnabled?: boolean
+    scrollEnabled?: boolean;
+
+    // If true, it will showcase adjacentViews
+    showAdjacentViews?: boolean;
+    // It indicates the visible width of adjacent-view
+    adjacentViewsWidth?: number;
+    // It works like margin between the adjacent-view
+    adjacentViewsPadding?: number;
+
+    decelerationRate?: 'fast' | 'normal' | number;
   }
 
   export default class Swiper extends Component<SwiperProps, SwiperState> {
-    scrollBy: (index?: number, animated?: boolean) => void
-    scrollTo: (index: number, animated?: boolean) => void
+    scrollBy: (index?: number, animated?: boolean) => void;
+    scrollTo: (index: number, animated?: boolean) => void;
   }
 }
